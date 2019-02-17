e0b22cf32701455998095104432e350baa354712
Claries Clarke <claries@Clariess-Air.fios-router.home>
2019-02-17 17:17:41 -0500

    initial commit

---
+++ audio.js
@@ -0,0 +1,26 @@
+
+  var going = document.getElementById("myAudio");
+  var fan = "temp";
+
+function run() {
+  going.play();
+}
+
+function stop() {
+  going.pause();
+}
+
+function reload() {
+  going.load();
+}
+
+function display() {
+  var get = document.getElementById("box");
+
+  }
+
+function file() {
+  console.log(fan);
+}
+
+console.log("hello");
